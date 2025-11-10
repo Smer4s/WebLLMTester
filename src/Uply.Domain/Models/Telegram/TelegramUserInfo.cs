058cc028d22a58f1c7e77811849ba53e5b450141
n.studnikita <studnikita.work@gmail.com>
2025-11-10 19:02:03 +0300

    fix: fixed user created

--- src/Uply.Domain/Models/Telegram/TelegramUserInfo.cs
+++ src/Uply.Domain/Models/Telegram/TelegramUserInfo.cs
@@ -1,9 +1,18 @@
-﻿namespace Uply.Domain.Models.Telegram;
+﻿using System.Text.Json.Serialization;
+
+namespace Uply.Domain.Models.Telegram;
 
 public record TelegramUserInfo
 {
+    [JsonPropertyName("id")]
     public long Id { get; set; }
+
+    [JsonPropertyName("username")]
     public string? Username { get; set; }
+
+    [JsonPropertyName("first_name")]
     public string FirstName { get; set; } = null!;
+
+    [JsonPropertyName("last_name")]
     public string? LastName { get; set; }
 }
